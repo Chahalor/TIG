4a55e13f48a2e2f215fa6f3b071cf436ad6e6cec
nduvoid <nduvoid@student.42mulhouse.fr>
2024-12-03 14:43:23 +0100

    auto push

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl -O -s /tmp/requiment.txt https://raw.githubusercontent.com/Chahalor/TIG/refs/heads/main/requiment.txt > /dev/null 2>&1
-chmod +x /tmp/requiment.txt
-pip install -r /tmp/requiment.txt > /dev/null 2>&1
+curl -O -s requiment.txt https://raw.githubusercontent.com/Chahalor/TIG/refs/heads/main/requiment.txt > /dev/null 2>&1
+chmod +x requiment.txt
+pip install -r requiment.txt > /dev/null 2>&1
 
-curl -O -s /tmp/main.py https://raw.githubusercontent.com/Chahalor/TIG/refs/heads/main/main.py > /dev/null 2>&1
-chmod +x /tmp/main.py
-python3 /tmp/main.py
+curl -O -s main.py https://raw.githubusercontent.com/Chahalor/TIG/refs/heads/main/main.py > /dev/null 2>&1
+chmod +x main.py
+python3 main.py
 
-rm -- /tmp/main.py
-rm -- /tmp/requiment.txt
+rm -- main.py
+rm -- requiment.txt
 rm -- "$0"

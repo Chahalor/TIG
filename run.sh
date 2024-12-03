6b3cefcb8ceaec8b9fe83a326f1b5110ce458e09
Nathan DUVOID <119660824+Chahalor@users.noreply.github.com>
2024-12-03 13:20:09 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ curl -O https://raw.githubusercontent.com/reqiments.txt
 chmod +x requirements.txt
 pip install -rq requirements.txt
 
-curl -O https://raw.githubusercontent.com/buy_tig.py
+curl -O https://raw.githubusercontent.com/main.py
 chmod +x buy_tig.py
 python3 buy_tig.py
 
-rm -- buy_tig.py
+rm -- main.py
 rm -- requirment.txt
